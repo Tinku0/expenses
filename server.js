c512067b381d0c785877bb7bdffcb9b50b3eefa8
Tinku0 <varunchowdary23@gmail.com>
2021-05-13 22:14:12 +0530

    cors added

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const cors = require('cors');
+
 const dotenv = require('dotenv');
 
 const mongoose = require('mongoose');
@@ -21,6 +23,7 @@ mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:
 const PORT = process.env.PORT || 5000;
 
 // middlewares
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended:true}));
 
